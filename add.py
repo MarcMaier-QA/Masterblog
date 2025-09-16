5df1863b4ca8b5faf7b19251db9e6c8695cca414
MarcMaier-QA <marc_maier1990@t-online.de>
2025-09-16 13:39:32 +0200

    Add 'Add Post' feature with /add route and form template

--- add.py
+++ add.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, request, redirect, url_for
+from utils import load_posts, save_posts
 
 app = Flask(__name__)
 
-def load_posts():
-    with open("posts.json", "r") as f:
-        return json.load(f)
-
-
 @app.route('/')
-def hello_world():
+def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        posts = load_posts()
+        new_post = {
+            "id": len(posts) + 1,
+            "author": request.form['author'],
+            "title": request.form['title'],
+            "content": request.form['content']
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
